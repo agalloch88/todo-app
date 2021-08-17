4b7a95ad62658197f73e2d9df73e8dea8502959b
Ryan Kirsch <ryan.kirsch@longevityconsulting.com>
2021-08-17 13:49:57 -0400

    refactor todos reducer, create new thunk to set up and store created todos in server

--- src/todos/thunks.js
+++ src/todos/thunks.js
@@ -1,4 +1,4 @@
-import { loadTodosInProgress, loadTodosSuccess, loadTodosFailure } from './actions';
+import { createTodo, loadTodosInProgress, loadTodosSuccess, loadTodosFailure } from './actions';
 
 export const loadTodos = () => async (dispatch, getState) => {
     try {
@@ -11,6 +11,24 @@ export const loadTodos = () => async (dispatch, getState) => {
         dispatch(loadTodosFailure());
         dispatch(displayAlert(error));
     }
+}
+
+export const addTodoRequest = (text) => async dispatch => {
+    try {
+        const body = JSON.stringify({ text });
+        const response = await fetch('http://localhost:8080/todos', {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        method: 'POST',
+        body,
+        });
+        const todo = await response.json();
+        dispatch(createTodo(todo));
+    } catch (error) {
+        dispatch(displayAlert(error));
+    }
+    
 }
 
 export const displayAlert = text => () => {
