9221da7d3173a01593ce8021f74e811969d7704d
Ryan Kirsch <ryan.kirsch@longevityconsulting.com>
2021-08-18 14:20:17 -0400

    add selectors for complete/incomplete todos

--- src/todos/selectors.js
+++ src/todos/selectors.js
@@ -1,3 +1,17 @@
+import { createSelector } from 'reselect';
+
 export const getTodos = state => state.todos.data;
 
 export const getTodosLoading = state => state.todos.isLoading;
+
+// using getIncompleteTodos as getIncompleteTodos(state) will return all todos existing in state which are incomplete
+
+export const getIncompleteTodos = createSelector(
+    getTodos,
+    (todos) => todos.filter(todo => !todo.isCompleted),
+);
+
+export const getCompletedTodos = createSelector(
+    getTodos,
+    (todos) => todos.filter(todo => todo.isCompleted),
+);
