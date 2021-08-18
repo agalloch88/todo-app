d74be719f7923efdc6037d6a1eb8c510cc03fd9c
Ryan Kirsch <ryan.kirsch@longevityconsulting.com>
2021-08-18 17:10:43 -0400

    create test for selectors, pass!

---
+++ src/todos/tests/selectors.test.js
@@ -0,0 +1,23 @@
+import { expect } from 'chain';
+import { getCompletedTodos } from '../selectors';
+
+describe('The getCompletedTodos selector', () => {
+    it('Returns only completed todos', () => {
+        const fakeTodos = [{
+            text: 'Say hello',
+            isCompleted: true,
+        }, {
+            text: 'Say goodbye',
+            isCompleted: false,
+        }, {
+            text: "Heyyyy",
+            isCompleted: false,
+        }];
+        const expected = [{
+            text: 'Say hello',
+            isCompleted: true,
+        }];
+        const actual = getCompletedTodos.resultFunc(fakeTodos);
+        expect(actual).to.deep.equal(expected);
+    });
+});
